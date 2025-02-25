43b76a93c3fb34966d9d74243ec4220f08cebec3
AbidHasanZidane <98080975+AbidHasanZidane@users.noreply.github.com>
2025-02-25 17:09:11 +0800

    Add files via upload

---
+++ C#/Homework1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace homework
+{ 
+    class Task
+    {
+        static void Main (string[] args)
+        {
+            int upperBound =10;
+            int lowerbound = 2;
+            Console.WriteLine("Check the whole numbers in between two digits, be nice and don't input anything funny, OK?");
+            Console.WriteLine("Enter the starting number:");
+            lowerbound = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the ending number:");
+            upperBound = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Numbers between " +lowerbound+" and "+ upperBound+" is: ");
+            for (int i = lowerbound; i<upperBound-1; )
+            {
+                Console.WriteLine(++i);
+            }
+        }
+    }
+}
+

cbd3d5671bc1fcbd9016f040e8522690ecf5ccac
AbidHasanZidane <98080975+AbidHasanZidane@users.noreply.github.com>
2025-02-25 22:06:09 +0800

    Add files via upload

---
+++ C#/Homework1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Class1
+    {
+        static void notMain(string[] args)
+        {
+            Console.WriteLine("Hello,Universe,我是 Zidane.");
+           
+        }
+        
+    }
+}
---
+++ C#/Homework1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace homework
+{ 
+    class Task
+    {
+        static bool IsPrime(int upperBound) //checks for prime number and returns a boolean value.
+        {
+            if(upperBound <=1)
+            {
+                return false;
+            }
+            for (int i = 2; i < upperBound; i++)
+            {
+                if(upperBound%i==0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main (string[] args)
+        {
+            int upperBound = 1; //gives error if they are not assigned, value assignment 
+            int lowerBound = 1;//in try catch blocks are not taken as granted by program.
+
+            Console.WriteLine("Check the prime numbers in between two digits. Be nice and don't input anything funny,just positive integers, OK?");
+            try
+            {
+                Console.WriteLine("Enter the starting number:");
+                lowerBound = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the ending number:");
+                upperBound = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Prime numbers between " + upperBound + " and " + lowerBound + " are:");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("You entered something weired,didn't you?");
+            }
+
+            int adjoustedUpperBound = upperBound - 1;
+
+            int count = 1;
+            while( adjoustedUpperBound>lowerBound)
+            {
+                if(IsPrime(adjoustedUpperBound)==false)
+                {
+                    adjoustedUpperBound--;
+                    
+                }
+                else
+                {
+                    Console.Write(adjoustedUpperBound+" ");
+                    adjoustedUpperBound--;
+                    if(count%10 == 0)
+                    {
+                        Console.WriteLine();
+                    }
+                    count++;
+                }
+                
+       
+            }
+           
+          
+           
+        }
+    }
+}
+
